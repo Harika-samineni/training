5d5905997fbde6a0bb289e9614b9d3590899f119
Harika-samineni <harikasaamineni@gmail.com>
2025-12-29 08:13:39 +0530

    code is updated

---
+++ source/test2.py
@@ -0,0 +1,19 @@
+print("Welcome to Billing App")
+product_name = input("Enter product name")
+price = int(input("enter price of product:")
+quantity = int(input("enter quantity:")
+
+
+total_cost = price*quantity
+gst = total_cost *0.1
+final_amount = total_costs + gst
+
+
+print("Bill details")
+print("product Name:", product_name)
+print("price:", price)
+print("Quantity:", quantity)
+print("Total Cost:", total_cost)
+print("GST (18%):", gst)
+print("Final Amount to pay:", final_amount)
+
